63c4aa7ccda463f24cc9947443e11ee765913572
grantr <grantr@gmail.com>
2010-06-10 11:51:09 -0700

    add admin methods to BaseProtocol

--- lib/elasticsearch/transport/base_protocol.rb
+++ lib/elasticsearch/transport/base_protocol.rb
@@ -25,10 +25,65 @@ module ElasticSearch
       end
 
       # admin index api
-      
+      def index_status(index_list, options={})
+        raise NotImplementedError
+      end
+
+      def create_index(index, create_options={}, options={})
+        raise NotImplementedError
+      end
+
+      def delete_index(index, options={})
+        raise NotImplementedError
+      end
+
+      def alias_index(operations, options={})
+        raise NotImplementedError
+      end
+
+      def update_mapping(index, type, mapping, options)
+        raise NotImplementedError
+      end
+
+      def flush(index_list, options={})
+        raise NotImplementedError
+      end
+
+      def refresh(index_list, options={})
+        raise NotImplementedError
+      end
+
+      def snapshot(index_list, options={})
+        raise NotImplementedError
+      end
+
+      def optimize(index_list, options={})
+        raise NotImplementedError
+      end
+
+
       # admin cluster api
+      def cluster_health(index_list, options={})
+        raise NotImplementedError
+      end
+
+      def cluster_state(options={})
+        raise NotImplementedError
+      end
 
       def nodes_info(node_list, options={})
+        raise NotImplementedError
+      end
+
+      def nodes_stats(node_list, options={})
+        raise NotImplementedError
+      end
+
+      def shutdown_nodes(node_list, options={})
+        raise NotImplementedError
+      end
+
+      def restart_nodes(node_list, options={})
         raise NotImplementedError
       end
 
