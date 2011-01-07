930d51222e89e1e5ee5c9d15228420c6138dae7a
grantr <grantr@gmail.com>
2011-01-07 11:49:10 -0800

    clarify test
    shoulda needs to support before(:all) and after(:all) like rspec

--- test/type_test.rb
+++ test/type_test.rb
@@ -2,24 +2,24 @@ require 'test_helper'
 
 class TypeTest < Test::Unit::TestCase
   context "Test with differents Types" do
+    #TODO figure out how to have one setup and one teardown
     setup do
-      client = ElasticSearch.new('127.0.0.1:9200', :index => "twitter", :type => "tweet")
-      client.index({:user => "kimchy"}, :id => 1)
-      client = ElasticSearch.new('127.0.0.1:9200', :index => "twitter", :type => "grillo")
-      client.index({:user => "kimchy"}, :id => 4)
-      client = ElasticSearch.new('127.0.0.1:9200', :index => "twitter", :type => "cote")
-      client.index({:user => "kimchy"}, :id => 3)
-      client = ElasticSearch.new('127.0.0.1:9200', :index => "cotes", :type => "cote")
-      client.index({:user => "kimchy"}, :id => 2)
-      client = ElasticSearch.new('127.0.0.1:9200', :index => "menchos", :type => "mencho")
-      client.index({:user => "kimchy"}, :id => 5)
-      client = ElasticSearch.new('127.0.0.1:9200', :index => "menchos", :type => "cote")
-      client.index({:user => "kimchy"}, :id => 6)
-      # I need sleep in order to refresh the indexes....at least 1 second?
-      sleep(1)
       @client = ElasticSearch.new('127.0.0.1:9200', :index => "twitter", :type => "tweet")
+      @client.index({:user => "kimchy"}, :id => 1)
+      @client.index({:user => "kimchy"}, :id => 2, :type => "grillo")
+      @client.index({:user => "kimchy"}, :id => 3, :type => "cote")
+      @client.index({:user => "kimchy"}, :id => 4, :index => "cotes", :type => "cote")
+      @client.index({:user => "kimchy"}, :id => 5, :index => "menchos", :type => "mencho")
+      @client.index({:user => "kimchy"}, :id => 6, :index => "menchos", :type => "cote")
+      @client.refresh("twitter", "cotes", "menchos")
     end
-    
+
+    teardown do
+      #@client.delete_index("twitter")
+      #@client.delete_index("cotes")
+      #@client.delete_index("menchos")
+    end
+
     should "search in all indexes" do
       assert_equal @client.count("kimchy",{:index => "", :type => ""}), 6
     end
