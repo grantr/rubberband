18d370ef674264466149e28390bd00ed8a13e3cd
Grant Rodgers <grantr@gmail.com>
2011-02-08 10:44:55 -0800

    use unique search term in types test
    hopefully avoids failures after incomplete test runs

--- test/type_test.rb
+++ test/type_test.rb
@@ -7,13 +7,14 @@ class TypeTest < Test::Unit::TestCase
       @first_index = 'first-' + Time.now.to_i.to_s
       @second_index = 'second-' + Time.now.to_i.to_s
       @third_index = 'third-' + Time.now.to_i.to_s
+      @username = 'kimchy' + Time.now.to_i.to_s
       @client = ElasticSearch.new('127.0.0.1:9200', :index => @first_index, :type => "tweet")
-      @client.index({:user => "kimchy"}, :id => 1)
-      @client.index({:user => "kimchy"}, :id => 2, :type => "grillo")
-      @client.index({:user => "kimchy"}, :id => 3, :type => "cote")
-      @client.index({:user => "kimchy"}, :id => 4, :index => @second_index, :type => "cote")
-      @client.index({:user => "kimchy"}, :id => 5, :index => @third_index, :type => "mencho")
-      @client.index({:user => "kimchy"}, :id => 6, :index => @third_index, :type => "cote")
+      @client.index({:user => @username}, :id => 1)
+      @client.index({:user => @username}, :id => 2, :type => "grillo")
+      @client.index({:user => @username}, :id => 3, :type => "cote")
+      @client.index({:user => @username}, :id => 4, :index => @second_index, :type => "cote")
+      @client.index({:user => @username}, :id => 5, :index => @third_index, :type => "mencho")
+      @client.index({:user => @username}, :id => 6, :index => @third_index, :type => "cote")
       @client.refresh(@first_index, @second_index, @third_index)
     end
 
@@ -25,20 +26,20 @@ class TypeTest < Test::Unit::TestCase
 
     should "Test different stages using indexes and types" do
       # Search in all indexes
-      assert_equal @client.count("kimchy",{:index => "", :type => ""}), 6
+      assert_equal @client.count(@username,{:index => "", :type => ""}), 6
       
       # Search in all types with index first
-      assert_equal @client.count("kimchy",{:index => @first_index, :type => ""}), 3
+      assert_equal @client.count(@username,{:index => @first_index, :type => ""}), 3
       
       # Search in first index with types tweet,cote
-      assert_equal @client.count("kimchy",{:index => @first_index, :type => "tweet,cote"}), 2
+      assert_equal @client.count(@username,{:index => @first_index, :type => "tweet,cote"}), 2
       
       # Search in index first and second
       @first_and_second = @first_index + ',' + @second_index  
-      assert_equal @client.count("kimchy",{:index => @first_and_second,  :type => ""}), 4
+      assert_equal @client.count(@username,{:index => @first_and_second,  :type => ""}), 4
       
       # Search in types grillo,cote of all indexes" do
-      assert_equal @client.count("kimchy",{:index => "",  :type => "grillo,cote"}), 4
+      assert_equal @client.count(@username,{:index => "",  :type => "grillo,cote"}), 4
     end
 
   end
