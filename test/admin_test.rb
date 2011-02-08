f2736e690ab639ab8c89359210c0069fa60a0375
Grant Rodgers <grantr@gmail.com>
2011-02-08 10:43:45 -0800

    admin test (just get/put mapping for now)

---
+++ test/admin_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class BasicTest < Test::Unit::TestCase
+  context "basic ops" do
+
+    setup do
+      @first_index = 'first-' + Time.now.to_i.to_s
+      @client = ElasticSearch.new('127.0.0.1:9200', :index => @first_index, :type => "tweet")
+    end
+
+    teardown do
+      @client.delete_index(@first_index)
+    end
+
+    should "get and update mappings" do
+      @client.index({:foo => "bar"}, :id => "1", :refresh => true)
+      
+      assert_equal({@first_index => {"tweet" => { "properties" => { "foo" => {"type" => "string" }}}}}, @client.index_mapping(@first_index))
+      @client.update_mapping({"tweet" => {:properties => {:bar => {:type => "string"}}}})
+      @client.refresh
+      assert_equal({@first_index => {"tweet" => { "properties" => { "foo" => {"type" => "string" }, "bar" => { "type" => "string"}}}}}, @client.index_mapping(@first_index))
+    end
+  end
+end
