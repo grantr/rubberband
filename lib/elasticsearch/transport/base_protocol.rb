636d8c27ef16744fe4a849163ca831ff8bcad08d
Grant Rodgers <grantr@gmail.com>
2011-02-07 20:31:14 -0800

    respect options passed to index, get, and delete

--- lib/elasticsearch/transport/base_protocol.rb
+++ lib/elasticsearch/transport/base_protocol.rb
@@ -4,16 +4,16 @@ module ElasticSearch
       def index(index, type, id, document, options={})
         body = encoder.is_encoded?(document) ? document : encoder.encode(document)
         if id.nil?
-          response = request(:post, {:index => index, :type => type}, {}, body)
+          response = request(:post, {:index => index, :type => type}, options, body)
         else
-          response = request(:put, {:index => index, :type => type, :id => id}, {}, body)
+          response = request(:put, {:index => index, :type => type, :id => id}, options, body)
         end
         handle_error(response) unless response.status == 200
         encoder.decode(response.body)
       end
 
       def get(index, type, id, options={})
-        response = request(:get, {:index => index, :type => type, :id => id})
+        response = request(:get, {:index => index, :type => type, :id => id}, options)
         return nil if response.status == 404
 
         handle_error(response) unless response.status == 200
@@ -24,7 +24,7 @@ module ElasticSearch
       end
 
       def delete(index, type, id, options={})
-        response = request(:delete,{:index => index, :type => type, :id => id})
+        response = request(:delete,{:index => index, :type => type, :id => id}, options)
         handle_error(response) unless response.status == 200 # ElasticSearch always returns 200 on delete, even if the object doesn't exist
         encoder.decode(response.body)
       end
