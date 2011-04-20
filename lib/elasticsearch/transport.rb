89d19185a01aef8eda84f60e2b2327529397abea
Dhruv Bansal <dhruv@ph.utexas.edu>
2011-04-20 17:14:26 -0500

    ConnectionFailed should subclass FatalError, not RetryableError.
    This avoids an infinite loop in ElasticSearch.new(...) when given bad/down servers.

--- lib/elasticsearch/transport.rb
+++ lib/elasticsearch/transport.rb
@@ -2,7 +2,7 @@ require "transport/base_protocol"
 require "transport/base"
 
 module ElasticSearch
-  class ConnectionFailed < RetryableError; end
+  class ConnectionFailed < FatalError; end
   class HostResolutionError < RetryableError; end
   class TimeoutError < RetryableError; end
   class RequestError < FatalError; end
