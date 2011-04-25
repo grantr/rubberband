cd2cd9e323475383a028d63eda96596dddb1389e
Grant Rodgers <grantr@gmail.com>
2011-04-25 14:51:49 -0700

    specs for connecting

---
+++ spec/connect_spec.rb
@@ -0,0 +1,32 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "connect" do
+
+  context 'one server' do
+    let(:servers) { '127.0.0.1:9200' }
+
+    it 'should connect' do
+      client = ElasticSearch.new(servers)
+      client.nodes_info.should include('cluster_name')
+    end
+  end
+
+  context "multiple servers" do
+  end
+ 
+  context 'invalid server' do
+    let(:servers) { '0.1.1.1:9200' }
+
+    it 'should raise ConnectionFailed' do
+      expect { ElasticSearch.new(servers).nodes_info }.to raise_error(ElasticSearch::ConnectionFailed)
+    end
+  end
+
+  context 'invalid servers' do
+    let(:servers) { ['0.1.1.1:9200', '0.2.2.2:9200'] }
+
+    it 'should raise ConnectionFailed' do
+      expect { ElasticSearch.new(servers).nodes_info }.to raise_error(ElasticSearch::ConnectionFailed)
+    end
+  end
+end
