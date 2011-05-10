bd15c193f263027fb39fdefa67f872ee57e8cde3
Grant Rodgers <grantr@gmail.com>
2011-05-10 12:17:47 -0700

    update sort docs to be consistent with recent es
    fixes #20

--- lib/elasticsearch/client/index.rb
+++ lib/elasticsearch/client/index.rb
@@ -58,7 +58,7 @@ module ElasticSearch
       #explain	 For each hit, contain an explanation of how to scoring of the hits was computed.
       #fields	 The selective fields of the document to return for each hit (fields must be stored), comma delimited. Defaults to the internal _source field.
       #field	 Same as fields above, but each parameter contains a single field name to load. There can be several field parameters.
-      #sort	 Sorting to perform. Can either be in the form of fieldName, or fieldName:reverse (for reverse sorting). The fieldName can either be an actual field within the document, or the special score name to indicate sorting based on scores. There can be several sort parameters (order is important).
+      #sort	 Sorting to perform. Can either be in the form of fieldName, or fieldName:desc (for reverse sorting). The fieldName can either be an actual field within the document, or the special score name to indicate sorting based on scores. There can be several sort parameters (order is important).
       #from	 The starting from index of the hits to return. Defaults to 0.
       #size	 The number of hits to return. Defaults to 10.
       #search_type	 The type of the search operation to perform. Can be dfs_query_then_fetch, dfs_query_and_fetch, query_then_fetch, query_and_fetch. Defaults to query_then_fetch.
